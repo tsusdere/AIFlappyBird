4e9d28f1eacbaac1df23d713380f277d07cad1d0
tsusdere <fishesandanime@gmail.com>
2020-11-25 15:34:46 -0600

    Final Commented code

--- AIFlappyBird.py
+++ AIFlappyBird.py
@@ -27,7 +27,7 @@ GEN =0
 
 '''
 Load the images to 
-create the bird and
+create the agent and
 enviroment
 '''
 #bird images to create animation
@@ -48,7 +48,16 @@ BACKGROUND = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs","bg.
 FONT = pygame.font.SysFont("comicsans", 50)
 
 
-#Bird object
+#---------------------------Bird----------------------------------------------------------#
+#class Bird
+#Purpose:
+#	The purpose of this class is to create the bird agent that will be used to apply
+#	the NEAT algorithm to. This class contains the settings and function that will
+#	allow the bird to move and such.
+#Notes:
+# - The bird contains multiple functions which weave together in order to produce a bird
+#   that is able to move and jump.
+#-----------------------------------------------------------------------------------------#
 class Bird:
     #Attributes of the bird
     IMGS = BIRDS
@@ -67,8 +76,7 @@ class Bird:
         self.img_count = 0
         self.img = self.IMGS[0]
 
-    #Jump setting for the bird
-    #to jump
+    #Jump settings for the bird
     def jump(self):
         self.vel = -10.5
         self.tick_count = 0
@@ -133,14 +141,28 @@ class Bird:
         center = rotated.get_rect(center=self.img.get_rect(topleft=(self.x,self.y)).center)
         win.blit(rotated,center.topleft)
 
-    #Colliders for the bird
+    #Colliders for the bird in the game
     def get_mask(self):
        return pygame.mask.from_surface(self.img)
-    
+
+
+#---------------------------Pipe------------------------------------------------#
+#class Pipe
+#Purpose:
+#	The purpose of this class is to create a collider in which if the 
+#	agent collides with it the agent will 'die'. This class contains
+#	functions that deal with moving the pipes in the map and also
+#	setting up the collider settings for them.
+#Notes:
+#	-In this class we set the colliders width and height and are able to 
+#	 vizualize it with a picture of a pipe
+#-------------------------------------------------------------------------------#
 class Pipe():
+    #Pipe settings
     GAP=200
     VEL=5 #Velocity of pipe
 
+    #Constructor
     def __init__(self,x): #No y because the height will be random
         self.x=x
         self.height=0
@@ -152,19 +174,22 @@ class Pipe():
         self.passed=False
         self.set_height()
         
-
-    def set_height(self): #Randomly define the top and bottom of the pipe
+    #Randomly define the top and bottom of the pipe
+    def set_height(self): 
         self.height=random.randrange(50,450)
         self.top=self.height-self.PIPE_TOP.get_height()
         self.bottom=self.height+self.GAP
-
+    
+    #Move the pipe 
     def move(self):
         self.x -= self.VEL
-
+    #Visualize the pipe in the game
     def draw(self,win):
         win.blit(self.PIPE_TOP,(self.x,self.top))
         win.blit(self.PIPE_BOTTOM,(self.x,self.bottom))
 
+    #Important function since this function will take care 
+    #of checking if the bird has collided with the pipe
     def collide(self, bird, win):
         bird_mask=bird.get_mask()
         top_mask=pygame.mask.from_surface(self.PIPE_TOP)
@@ -179,17 +204,28 @@ class Pipe():
         if t_point or b_point:
             return True
         return False
-
+#---------------------------Base--------------------------------------------#
+#class Base
+#Purpose:
+#	The purpose of this class is to draw the bottom of the map in the 
+#	game and to give it an effect that it's 'moving' alongside the bird.
+#	This class contains functions that involve drawing the base in the
+#	game and also moving it.
+#--------------------------------------------------------------------------#
 class Base:
+    #Settings for the base
     VEL=5
     WIDTH=BASE.get_width()
     IMG=BASE
 
+    #Constructor
     def __init__(self,y): #No x becase it moves to the left
         self.y=y
         self.x1=0 #Start first image at 0
         self.x2=self.WIDTH #Start second image behind the first
 
+    #Move the base of the game to give it the effect that it's 
+    #moving in order to create the effect of the bird moving
     def move(self):
         self.x1 -= self.VEL #Image 1
         self.x2 -= self.VEL #Image 2
@@ -201,31 +237,66 @@ class Base:
         if self.x2 + self.WIDTH < 0:
             self.x2 = self.x1 + self.WIDTH
 
+    #Draw the base in the game
     def draw(self,win):
         win.blit(self.IMG,(self.x1,self.y))
         win.blit(self.IMG,(self.x2,self.y))
 
+#---------------------draw_win--------------------------------#
+#def draw_win(win,birds,pipes,base,score , gen):
+#Purpose:
+#	The purpose of this function is to draw the game 
+#	window and to show the game in the display.
+#Parameters:
+# Win		Window to display the game
+# Birds		Array of bird agents
+# Pipes		Array of pipes
+# Score		Current score 
+# Gen		Curretn generation
+#Returns: N/A
+#Notes:
+#    -This function will be called every frame in the game
+#     this is due to how pygame works and thus all of these
+#     paramters will be updated every frame of the game
+#------------------------------------------------------------#
 def draw_win(win,birds,pipes,base,score , gen):
     win.blit(BACKGROUND, (0,0))
-
+    #Draw all the pipes
     for pipe in pipes:
         pipe.draw(win)
-
+    #Render the score in the screen aswell as the generation
     text=FONT.render("Score: " + str(score),1,(255,255,255))
     win.blit(text,(WIDTH - 10 - text.get_width(),10))
-
     text=FONT.render("Gen: " + str(gen),1,(255,255,255))
     win.blit(text,( 10 ,10))
-
+    #Draw the base 
     base.draw(win)
-
+    #Draw all the birds in the window
     for bird in birds:
         bird.draw(win)
 
     #bird.draw(win)
     pygame.display.update()
 
+#------------------------main--------------------------------------#
+#def main(genomes,config)
+#Purpose:
+#	The purpose of this function is to run the program and 
+#	to apply the NEAT algorithm on the number of genomes
+#	that we specified. This function will also deal with 
+#	drawing the game into the window.
+#Parameters:
+# genomes		The number of genomes 
+# config		The NEAT configurations
+#Returns: N/A
+#Notes:
+#	-This will be the bread and butter of the program in the 
+#	 sense that this function weaves everything together nicely
+#-------------------------------------------------------------------#
 def main(genomes,config):
+    
+    #Set global settings for the generation
+    #and the display window we will use
     global GEN
     GEN +=1
     win = pygame.display.set_mode((WIDTH,HEIGHT))
@@ -236,22 +307,29 @@ def main(genomes,config):
     #bird=Bird(230,350)
     birds=[] #Create multiple birds
 
+    #Traverse the genome list
     for _, g in genomes:
         net=neat.nn.FeedForwardNetwork.create(g,config) #Create the neural network
         nets.append(net) #Append NN to the list
         birds.append(Bird(230,350)) #Create a bird object that starts at the same position as other birds
         g.fitness=0 #Initial fitness
-        ge.append(g)
+        ge.append(g) #Append the bird to the ge list
 
+    #Enviroment settings for the game 
     base=Base(730)
     pipes=[Pipe(650)]
     win = pygame.display.set_mode((WIDTH, HEIGHT))
     clock = pygame.time.Clock()
     score=0
 
+    #Keep the game going until run is equal to False
+    #this will occur when the bird has died
     run=True
     while run:
+	#Number of frames we will be running the game at
         clock.tick(30)
+        #check if we have quit the game by pressing the 
+	#X on the top corner
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
                 run=False
@@ -268,7 +346,7 @@ def main(genomes,config):
         else:
             run = False
             break
-
+	#Traverse all the birds in the list and use our neural network
         for x, bird in enumerate(birds):
             bird.move()
             ge[x].fitness += 0.1 #Give the bird some fitness so it survives for a little while
@@ -280,10 +358,13 @@ def main(genomes,config):
                 bird.jump()
 
         #bird.move()
+        #Add the pipes and traverse the pipes in the list
+        #we also check if we have collided with any of the pipes
         add_pipe=False
         rem=[]
         for pipe in pipes:
             for x, bird in enumerate(birds):
+		#Check for bird collision with pipe
                 if pipe.collide(bird,win):
                     ge[x].fitness -= 1 #If the bird hits the pipe, deduct the score
                     #Remove underperforming birds and don't use them in the next interation
@@ -297,13 +378,13 @@ def main(genomes,config):
                     add_pipe=True
 
             #If pipe moves off screen append them to the removed list
-            
-
             if pipe.x + pipe.PIPE_TOP.get_width() < 0:
                 rem.append(pipe)
 
             pipe.move()
 
+	#Check if the bird was able to pass through
+	#the pipe and add 1 point to the score
         if add_pipe:
 
             score += 1
@@ -322,26 +403,40 @@ def main(genomes,config):
                 birds.pop(x)
                 nets.pop(x)
                 ge.pop(x)
-
-
-
-            
+        #Make the base appear as it's moving and call 
+        #the draw_win function to update our UI    
         base.move()
         draw_win(win,birds,pipes,base,score, GEN)
 
+#-------------------------------run------------------------------------#
+#def run(config_path)
+#Purpose:
+#	The purpose of this function is to get the NEAT configurations
+#	and to determine based on the configurations and to run the
+#	game.
+#Parameters:
+# config_path	 	A string path to a NEAT configuration file
+#Returns: N/A
+#Notes:
+#	- The path to the configuration file must exist and it must
+#	  be passed as a string
+#----------------------------------------------------------------------#
 def run(config_path):
+    #Load the NEAT settings
     config=neat.config.Config(neat.DefaultGenome,neat.DefaultReproduction,neat.DefaultSpeciesSet,neat.DefaultStagnation,config_path)
-
     p=neat.Population(config)
-
     p.add_reporter(neat.StdOutReporter(True))
     stats=neat.StatisticsReporter()
     p.add_reporter(stats)
 
+    #Run the game with a bird population of 50
+    #and get the winner
     winner=p.run(main,50)
 
 
 if __name__ == "__main__":
+    #Load the configuration file and run the game
+    #with the configurations chosen
     local_dir=os.path.dirname(__file__)
     config_path=os.path.join(local_dir,"configFile.txt")
     run(config_path)
